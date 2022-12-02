a8b66840b81860461367bfe9d4c319c99f554249
Angelina <angelinabobrova@yandex.ru>
2022-12-02 13:40:32 +0400

    add final project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,9 @@ return (
     <header className="Header">
         <h1 className="Header-name">NEW SHOPPING GALLERY</h1>
     <div className="Header-container">
-        <a href="http://localhost:3000/4p22-final-project-angelina-volkova" target="" className="Header-container__link">About</a>
-        <a href="http://localhost:3000/4p22-final-project-angelina-volkova" target="" className="Header-container__link">Payment</a>
-        <a href="http://localhost:3000/4p22-final-project-angelina-volkova" target="" className="Header-container__link">Delivery</a>
+        <Link to={''} className="Header-container__link">About</Link>
+        <Link to={''} className="Header-container__link">Payment</Link>
+        <Link to={''} className="Header-container__link">Delivery</Link>
         <a href="#contacts" className="Header-container__link">Contacts</a>
         <Link to={'contacts'} className="Header-container__link" >Feedback</Link> 
         <CustomizedInputBase />
