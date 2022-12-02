e3a8d25a27cd389b9b860374bd642e6136b70ef0
Angelina <angelinabobrova@yandex.ru>
2022-12-02 12:05:06 +0400

    add final priject

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,9 +7,7 @@ function Footer () {
             <a href="tel:+7123456789"className="Footer-link">+7123456789</a> <br />
             <a href= "email:example@ya.ru" className="Footer-link">example@ya.ru</a>
             <p><em>© Volkova 2022</em></p>
-    
-</footer>  
-
-    )
+        </footer>  
+)
 }
 export default Footer;
